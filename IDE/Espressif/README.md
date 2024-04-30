81bf40b550604373d3ca70a961c34a15ebb300a1
gojimmypi <jim@wolfssl.com>
2024-04-29 18:21:19 -0700

    Initial compiling Espressif for wolfTPM (not implemented)

---
+++ IDE/Espressif/README.md
@@ -0,0 +1,12 @@
+# wolfTPM for Espressif
+
+## TODO
+
+Currently the `components\wolftpm\include\options.h` is not found by the respective component `CMakeLists.txt`, 
+so it is copied to `C:\workspace\wolfTPM-gojimmypi\wolftpm` as an interim solution.
+
+The `wrap_test.h` is currently _copied_ to the example. Consider pointing to original repo source.
+
+```
+./configure --enable-infineon --enable-i2c --with-wolfcrypt=[path]
+```
---
+++ IDE/Espressif/main/main.c
@@ -0,0 +1,78 @@
+/* wolftpm_test.c
+ *
+ * Copyright (C) 2014-2023 wolfSSL Inc.
+ *
+ * This file is part of wolfTPM.
+ *
+ * wolfTPM is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * wolfTPM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with wolfTPM.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* Espressif */
+#include <esp_log.h>
+
+/* wolfSSL */
+/* Always include wolfcrypt/settings.h before any other wolfSSL file.    */
+/* Reminder: settings.h pulls in user_settings.h; don't include it here. */
+#ifdef WOLFSSL_USER_SETTINGS
+    #include <wolfssl/wolfcrypt/settings.h>
+    #ifndef WOLFSSL_ESPIDF
+        #warning "Problem with wolfSSL user_settings."
+        #warning "Check components/wolfssl/include"
+    #endif
+    #include <wolfssl/wolfcrypt/port/Espressif/esp32-crypt.h>
+#else
+    /* Define WOLFSSL_USER_SETTINGS project wide for settings.h to include   */
+    /* wolfSSL user settings in ./components/wolfssl/include/user_settings.h */
+    #error "Missing WOLFSSL_USER_SETTINGS in CMakeLists or Makefile:\
+    CFLAGS +=-DWOLFSSL_USER_SETTINGS"
+#endif
+
+
+/* project */
+#include "main.h"
+
+static const char* const TAG = "My Project";
+
+extern int TPM2_Wrapper_Test(void* userCtx);
+
+void app_main(void)
+{
+    int ret = 0;
+    int * argument = NULL;
+    ESP_LOGI(TAG, "Hello wolfSSL!");
+
+#ifdef HAVE_VERSION_EXTENDED_INFO
+    ret = esp_ShowExtendedSystemInfo();
+#endif
+
+#if defined(WOLFSSL_HW_METRICS) && defined(WOLFSSL_HAS_METRICS)
+    ret += TPM2_Wrapper_Test((void*)argument);
+#endif
+
+#ifdef WOLFSSL_ESPIDF_VERBOSE_EXIT_MESSAGE
+    if (ret == 0) {
+        ESP_LOGI(TAG, WOLFSSL_ESPIDF_VERBOSE_EXIT_MESSAGE("Success!", ret));
+    }
+    else {
+        ESP_LOGE(TAG, WOLFSSL_ESPIDF_VERBOSE_EXIT_MESSAGE("Failed!", ret));
+    }
+#elif defined(WOLFSSL_ESPIDF_EXIT_MESSAGE)
+    ESP_LOGI(TAG, WOLFSSL_ESPIDF_EXIT_MESSAGE);
+#else
+    ESP_LOGI(TAG, "\n\nDone!"
+                  "If running from idf.py monitor, press twice: Ctrl+]\n\n"
+                  "WOLFSSL_COMPLETE\n" /* exit keyword for wolfssl_monitor.py */
+            );
+#endif
+}
